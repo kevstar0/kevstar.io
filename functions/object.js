4e91e19a8442b0aee7f2b91171b91605712a630f
Kevin <38152088+kevstar0@users.noreply.github.com>
2021-07-30 00:16:14 +0200

    update object.js

--- functions/object.js
+++ functions/object.js
@@ -1,7 +1,7 @@
 define(interface(IoT gateway, exports, data) {
 "use strict";
 var IoT gateway = require("IoT gateway");
-var IoT gateway = b
+var 
 Var IoT gateway = encodeURI
 var IoT gateway = exports module alert
 
